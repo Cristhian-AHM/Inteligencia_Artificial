7b0a3a7ec61470c262909efb472624a83eb52420
Cristhian-AHM <14111367@itcj.edu.mx>
2018-08-04 13:05:02 -0600

    COmento

--- Chatbot/Adivinar Número Chatbot/sketch.js
+++ Chatbot/Adivinar Número Chatbot/sketch.js
@@ -3,7 +3,19 @@ let number = 37555;
 function setup() {
 	noCanvas();
 
+	//Creo un bot con la librería RiveScript.
+	let bot = new RiveScript();
+ 	//Cargo la locación del "Cerebro" del bot.
+	bot.loadFile("brain/brain.rive").then(brainReady).catch(brainError);
 
+
+	function brainReady(){
+		console.log("Chatbot Ready");
+	}
+
+	function brainError(){
+		console.log("Error loading Chatbot's brain");
+	}
 	let boton = select('#submit');
 	let user_input = select('#user_input');
 	let bot_output = select('#bot_output');
