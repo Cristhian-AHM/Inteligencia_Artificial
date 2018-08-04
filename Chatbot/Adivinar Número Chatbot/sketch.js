918c095f92ae48e11a8417cd4d45ca5f976cd7fc
Cristhian-AHM <14111367@itcj.edu.mx>
2018-08-04 13:22:42 -0600

    Agregando Respuesta del chatbot

--- Chatbot/Adivinar Número Chatbot/sketch.js
+++ Chatbot/Adivinar Número Chatbot/sketch.js
@@ -1,29 +1,36 @@
 let number = 37555;
 
 function setup() {
-	noCanvas();
+  noCanvas();
 
-	//Creo un bot con la librería RiveScript.
-	let bot = new RiveScript();
- 	//Cargo la locación del "Cerebro" del bot.
-	bot.loadFile("brain/brain.rive").then(brainReady).catch(brainError);
+  //Creo un bot con la librería RiveScript.
+  let bot = new RiveScript();
 
+  //Cargo la locación del "Cerebro" del bot.
+  bot.loadFile("brain/brain.rive").then(brainReady).catch(brainError);
 
-	function brainReady(){
-		console.log("Chatbot Ready");
-	}
+  //Funcón que indica que el cerebro del bot se cargo correctamente.
+  function brainReady() {
+    console.log("Chatbot Ready");
+    bot.sortReplies();
+  }
 
-	function brainError(){
-		console.log("Error loading Chatbot's brain");
-	}
-	let boton = select('#submit');
-	let user_input = select('#user_input');
-	let bot_output = select('#bot_output');
+  //Funcón que indica que el cerebro del bot tuvo problema al cargarse.
+  function brainError() {
+    console.log("Error loading Chatbot's brain");
+  }
+  let boton = select('#submit');
+  let user_input = select('#user_input');
+  let bot_output = select('#bot_output');
 
-	boton.mousePressed(chat);
+  boton.mousePressed(chat);
 
-	function chat(){
-		let entrada = user_input.value();
-		bot_output.html(entrada);
-	}
+  function chat() {
+    let entrada = user_input.value();
+    var respuesta;
+    bot.reply("local-user", "Hello, bot!").then(function(reply) {
+	    bot_output.html(reply);
+    });
+
+  }
 }
