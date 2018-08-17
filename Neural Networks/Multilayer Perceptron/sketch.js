402a4744e600b87d86203bda131b1d1aba276fd1
Cristhian-AHM <14111367@itcj.edu.mx>
2018-08-16 18:56:08 -0600

    Se agrego una visualización de como resuelve XOR.

--- Neural Networks/Multilayer Perceptron/sketch.js
+++ Neural Networks/Multilayer Perceptron/sketch.js
@@ -1,39 +1,48 @@
-
-let training_data = [
-	{
-		inputs: [0,1],
-		targets: [1]
-	},
-	{
-		inputs: [1,0],
-		targets: [1]
-	},
-	{
-		inputs: [0,0],
-		targets: [0]
-	},
-	{
-		inputs: [1,1],
-		targets: [0]
-	}
+let training_data = [{
+    inputs: [0, 1],
+    targets: [1]
+  },
+  {
+    inputs: [1, 0],
+    targets: [1]
+  },
+  {
+    inputs: [0, 0],
+    targets: [0]
+  },
+  {
+    inputs: [1, 1],
+    targets: [0]
+  }
 ];
+let nn;
+
 function setup() {
-	let nn = new NeuralNetwork(2,2,1);
+  createCanvas(400, 400);
+  nn = new NeuralNetwork(2, 2, 1);
+}
 
-	for (var i = 0; i < 100000; i++) {
-		let data = random(training_data);
-			nn.train(data.inputs, data.targets);
+function draw() {
+  background(0);
+	//Entreno a la red 100 cada vez que re-dibuja la pantalla.
+  for (let i = 0; i < 100; i++) {
+    let data = random(training_data);
+    nn.train(data.inputs, data.targets);
+  }
+	//Se crea una maya.
+	let resolution = 10;
+	let columns = width / resolution;
+	let rows = height / resolution;
 
+	for (var i = 0; i < columns; i++) {
+		for (var j = 0; j < rows; j++) {
+			//Genera que la esquina superior izqueirda sea 0,0 y la esquina inferior derecha sea 1,1
+			let x1 = i / columns;
+			let x2 = j / rows;
+			let inputs = [x1, x2];
+			let y = nn.predict(inputs);
+			fill(y * 255);
+			rect(i * resolution, j * resolution, resolution, resolution);
+		}
 	}
-
-	console.log("XOR");
-	console.log("1 0: " + nn.predict([1,0]));
-	console.log("0 1: " +nn.predict([0,1]));
-	console.log("0 0: " +nn.predict([0,0]));
-	console.log("1 1: " +nn.predict([1,1]));
-
-	console.log(nn.serialize());
-
-	//console.log(nn.train(input, targets));
-	//console.log(output);
 }
